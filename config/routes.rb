a9c7e0de8325edb24d11f16d0632dbd4d268ef15
Isabel Cheung <isabelczyi@gmail.com>
2022-02-24 14:14:01 +1000

    all basic functionalities for lists and bookmarks but poor styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # get 'bookmarks/new'
+  # get 'bookmarks/create'
+  # get 'lists/index'
+  # get 'lists/new'
+  # get 'lists/show'
+  # get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :new, :show, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
